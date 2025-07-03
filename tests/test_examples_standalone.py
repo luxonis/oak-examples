87bc0749bb35e8bdc653814dfcccfa1204a547ef
Danilo <danilo.pejovic@luxonis.com>
2025-07-03 21:08:14 +0200

    fix

--- tests/test_examples_standalone.py
+++ tests/test_examples_standalone.py
@@ -101,7 +101,7 @@ def run_example(example_dir: Path, args: dict) -> bool:
     connect_timeout = 60
     try:
         result = subprocess.run(
-            ["oakctl", f"--password {args['device_password']}", "connect", "info"],
+            ["oakctl", "--password ", args["device_password"], "device", "info"],
             check=True,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
@@ -126,7 +126,7 @@ def run_example(example_dir: Path, args: dict) -> bool:
         logger.debug(f"Installing {example_dir} app")
 
         process = subprocess.Popen(
-            ["oakctl", f"--password {args['device_password']}", "app", "run", "."],
+            ["oakctl", "--password ", args["device_password"], "app", "run", "."],
             stdout=subprocess.PIPE,
             stderr=subprocess.STDOUT,
             text=True,
@@ -175,7 +175,7 @@ def run_example(example_dir: Path, args: dict) -> bool:
             except queue.Empty:
                 pass
 
-            status = get_app_status(APP_ID)
+            status = get_app_status(APP_ID, args)
             # When app has started, check if it exited early
             if status != "running":
                 logger.error(
@@ -208,10 +208,10 @@ def run_example(example_dir: Path, args: dict) -> bool:
         return False
 
 
-def get_app_status(app_id: str):
+def get_app_status(app_id: str, args: dict):
     try:
         result = subprocess.run(
-            ["oakctl", "app", "list", "--format=json"],
+            ["oakctl", "--password ", args["device_password"], "app", "list", "--format=json"],
             check=True,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
@@ -240,7 +240,7 @@ def teardown(args: dict):
     # Delete app on device
     try:
         result = subprocess.run(
-            ["oakctl", f"--password {args['device_password']}", "app", "delete", APP_ID],
+            ["oakctl", "--password ", args["device_password"], "app", "delete", APP_ID],
             check=True,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
