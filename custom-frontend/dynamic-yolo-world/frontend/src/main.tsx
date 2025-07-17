aad78813317f6b24913b5ec7b0294aaa0ac8c252
Jindrich Flidr <jindrich.flidr@luxonis.com>
2025-07-17 11:43:14 +0200

    fix yolo world frontend

--- custom-frontend/dynamic-yolo-world/frontend/src/main.tsx
+++ custom-frontend/dynamic-yolo-world/frontend/src/main.tsx
@@ -8,9 +8,15 @@ import App from './App.tsx';
 import {BrowserRouter, Route, Routes} from "react-router";
 import {DepthAIContext} from "@luxonis/depthai-viewer-common";
 
+// This function extracts the base path with app version from the current URL.
+// This is essential for access via domain luxonis.app
+function getBasePath(): string {
+  return window.location.pathname.match(/^\/\d+\.\d+\.\d+\/$/)?.[0] ?? ''
+}
+
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-            <BrowserRouter basename={window.__basePath}>
+            <BrowserRouter basename={getBasePath()}>
                 <DepthAIContext activeServices={
                     // @ts-ignore - We're using an example service here which isn't part of the DAI services enum
                     ['Custom Service']
