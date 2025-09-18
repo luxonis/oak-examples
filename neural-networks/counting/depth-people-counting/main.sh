9fea428fa5341a26685e3fe3c461f4dd5c2f8c51
jkbmrz <jakob.mraz@gmail.com>
2025-09-18 10:44:18 +0200

    fix: remove bash file

--- neural-networks/counting/depth-people-counting/main.sh
+++
@@ -1,9 +0,0 @@
-python main_jakob.py \
-    --device 10.12.143.167
-#    --device 10.12.118.96
-#    --device 3.1
-#    --device 10.12.119.75
-#    --media /home/struga/Desktop/tmp/20250114_rewrite_adcanced_experiments/videos/people_smiling.mp4 \
-#    
-#    
-#    
