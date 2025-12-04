f62749c1b084f1d744ed6b3a3b40727a575b6b29
Danilo <danilo.pejovic@luxonis.com>
2025-12-04 18:55:28 +0100

    standalone mac

--- tests/run_tests_peripheral.sh
+++ tests/run_tests_peripheral.sh
@@ -1,29 +1,19 @@
 #!/usr/bin/env bash
-# -----------------------------------------------------------
-# EXPECTED ARGUMENTS:
-#   $1 = PYTHON_VERSION_ENV
-#   $2 = DAI_VERSION
-#   $3 = DAI_NODES_VERSION
-#   $4 = PLATFORM
-#   $5 = STRICT_MODE
-#   $6 = ROOT_DIR
-#   $7 = LOG_LEVEL
-# -----------------------------------------------------------
 
 set -euo pipefail
 
 if [[ "${4:-}" == "" ]]; then
-  echo "Usage: ./run_tests.sh PYTHON_VERSION_ENV DAI_VERSION DAI_NODES_VERSION PLATFORM STRICT_MODE ROOT_DIR LOG_LEVEL"
+  echo "Usage: ./run_tests.sh PYTHON_VERSION_ENV PLATFORM STRICT_MODE ROOT_DIR LOG_LEVEL DAI_VERSION DAI_NODES_VERSION"
   exit 1
 fi
 
 PYTHON_VERSION_ENV="$1"
-DAI_VERSION="$2"
-DAI_NODES_VERSION="$3"
-PLATFORM="$4"
-STRICT_MODE="$5"
-ROOT_DIR="$6"
-LOG_LEVEL="$7"
+PLATFORM="$2"
+STRICT_MODE="$3"
+ROOT_DIR="$4"
+LOG_LEVEL="$5"
+DAI_VERSION="$6"
+DAI_NODES_VERSION="$7"
 
 echo "=========================================="
 echo "Running tests with:"
@@ -57,4 +47,4 @@ pytest -v -r a --log-cli-level="${LOG_LEVEL}" --log-file=out.log --color=yes \
   --python-version="${PYTHON_VERSION_ENV}" \
   --strict-mode="${STRICT_MODE}" \
   --root-dir "${ROOT_DIR}" \
-  -q tests/test_examples_peripheral.py"
+  -q tests/test_examples_peripheral.py
